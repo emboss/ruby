47583b84d6504bc15e6ed00bec29039be57cde06
emboss <Martin.Bosslet@googlemail>
2011-07-01 17:58:29 +0200

    Playing around with libxml

---
+++ ext/openssl/xmldsig/scratch/main.c
@@ -0,0 +1,128 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <assert.h>
+
+#include <libxml/tree.h>
+#include <libxml/parser.h>
+#include <libxml/xpath.h>
+#include <libxml/xpathInternals.h>
+
+int  execute_xpath_expression();
+int  register_namespaces(xmlXPathContextPtr xpathCtx, const xmlChar* nsList);
+void print_xpath_nodes(xmlNodeSetPtr nodes, FILE* output);
+
+int
+main(int argc, char **argv) {
+    /* Init libxml */
+    xmlInitParser();
+    LIBXML_TEST_VERSION
+
+    execute_xpath_expression();
+
+    /* Shutdown libxml */
+    xmlCleanupParser();
+
+    /*
+     * this is to debug memory for regression tests
+     */
+    xmlMemoryDump();
+    return 0;
+}
+
+int
+execute_xpath_expression() {
+    xmlDocPtr doc;
+    xmlXPathContextPtr xpathCtx;
+    xmlXPathObjectPtr xpathObj;
+    const xmlChar *xpathExpr = "//xhtml:div";
+    
+    /* Load XML document */
+    doc = xmlParseFile("example.xhtml");
+    if (doc == NULL) {
+	fprintf(stderr, "Error: unable to parse file example.xhtml\n");
+	return(-1);
+    }
+
+    /* Create xpath evaluation context */
+    xpathCtx = xmlXPathNewContext(doc);
+    if(xpathCtx == NULL) {
+        fprintf(stderr,"Error: unable to create new XPath context\n");
+        xmlFreeDoc(doc);
+        return(-1);
+    }
+
+    if (xmlXPathRegisterNs (xpathCtx, (xmlChar*)"xhtml", (xmlChar*)"http://www.w3.org/1999/xhtml") < 0) {
+        fprintf(stderr,"Error: failed to register namespace\n");
+        xmlXPathFreeContext(xpathCtx);
+        xmlFreeDoc(doc);
+        return(-1);
+    }
+
+    /* Evaluate xpath expression */
+    xpathObj = xmlXPathEvalExpression(xpathExpr, xpathCtx);
+    if(xpathObj == NULL) {
+        fprintf(stderr,"Error: unable to evaluate xpath expression \"%s\"\n", xpathExpr);
+        xmlXPathFreeContext(xpathCtx);
+        xmlFreeDoc(doc);
+        return(-1);
+    }
+
+    /* Print results */
+    print_xpath_nodes(xpathObj->nodesetval, stdout);
+
+    /* Cleanup */
+    xmlXPathFreeObject(xpathObj);
+    xmlXPathFreeContext(xpathCtx);
+    xmlFreeDoc(doc);
+
+    return(0);
+}
+
+/**
+ * print_xpath_nodes:
+ * @nodes:		the nodes set.
+ * @output:		the output file handle.
+ *
+ * Prints the @nodes content to @output.
+ */
+void
+print_xpath_nodes(xmlNodeSetPtr nodes, FILE* output) {
+    xmlNodePtr cur;
+    int size;
+    int i;
+
+    assert(output);
+    size = (nodes) ? nodes->nodeNr : 0;
+
+    fprintf(output, "Result (%d nodes):\n", size);
+    for(i = 0; i < size; ++i) {
+	assert(nodes->nodeTab[i]);
+
+	if(nodes->nodeTab[i]->type == XML_NAMESPACE_DECL) {
+	    xmlNsPtr ns;
+
+	    ns = (xmlNsPtr)nodes->nodeTab[i];
+	    cur = (xmlNodePtr)ns->next;
+	    if(cur->ns) {
+	        fprintf(output, "= namespace \"%s\"=\"%s\" for node %s:%s\n",
+		    ns->prefix, ns->href, cur->ns->href, cur->name);
+	    } else {
+	        fprintf(output, "= namespace \"%s\"=\"%s\" for node %s\n",
+		    ns->prefix, ns->href, cur->name);
+	    }
+	} else if(nodes->nodeTab[i]->type == XML_ELEMENT_NODE) {
+	    cur = nodes->nodeTab[i];
+	    if(cur->ns) {
+    	        fprintf(output, "= element node \"%s:%s\"\n",
+		    cur->ns->href, cur->name);
+	    } else {
+    	        fprintf(output, "= element node \"%s\"\n",
+		    cur->name);
+	    }
+	} else {
+	    cur = nodes->nodeTab[i];
+	    fprintf(output, "= node \"%s\": type %d\n", cur->name, cur->type);
+	}
+    }
+}
---
+++ ext/openssl/xmldsig/scratch/nbproject/Makefile-variables.mk
@@ -0,0 +1,24 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=GNU-Linux-x86
+CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-Linux-x86
+CND_ARTIFACT_NAME_Debug=scratch
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-Linux-x86/scratch
+CND_PACKAGE_DIR_Debug=dist/Debug/GNU-Linux-x86/package
+CND_PACKAGE_NAME_Debug=scratch.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-Linux-x86/package/scratch.tar
+# Release configuration
+CND_PLATFORM_Release=GNU-Linux-x86
+CND_ARTIFACT_DIR_Release=dist/Release/GNU-Linux-x86
+CND_ARTIFACT_NAME_Release=scratch
+CND_ARTIFACT_PATH_Release=dist/Release/GNU-Linux-x86/scratch
+CND_PACKAGE_DIR_Release=dist/Release/GNU-Linux-x86/package
+CND_PACKAGE_NAME_Release=scratch.tar
+CND_PACKAGE_PATH_Release=dist/Release/GNU-Linux-x86/package/scratch.tar
