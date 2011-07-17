a842742247f678025319734163b4f75b34bd55a4
Martin Boßlet <Martin.Bosslet@googlemail.com>
2011-07-17 17:01:50 +0200

    Implement document parsing and re-encoding

---
+++ test/xmldsig/test_doc.rb
@@ -0,0 +1,36 @@
+# encoding: UTF-8
+require_relative 'utils'
+
+class TestDocument < XmlDsig::TestCase
+
+  def test_encoding_utf8_default
+    raw = <<-_EOS_
+<?xml version="1.0"?>
+<root><element/></root>
+_EOS_
+    doc = XmlDsig::Document.new(raw)
+    bytes = doc.bytes
+    assert_equal("UTF-8", bytes.encoding.name)
+  end
+
+  def test_encoding_utf8
+    doc = XmlDsig::Document.new(XmlDsig::TestUtils::TEST_DOC_UTF_8)
+    bytes = doc.bytes
+    assert_equal("UTF-8", bytes.encoding.name)
+  end
+
+  def test_encoding_iso_8859_1
+    doc = XmlDsig::Document.new(XmlDsig::TestUtils::TEST_DOC_ISO_8859_1)
+    bytes = doc.bytes
+    assert_equal("ISO-8859-1", bytes.encoding.name)
+  end
+
+  def test_encoding_us_ascii
+    doc = XmlDsig::Document.new(XmlDsig::TestUtils::TEST_DOC_US_ASCII)
+    bytes = doc.bytes
+    assert_equal("US-ASCII", bytes.encoding.name)
+  end
+
+
+end
+
